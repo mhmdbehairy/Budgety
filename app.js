0edf28e10aebb33e4ff1ec99828229c46beb8acb
mhmdbehairy <mhmd.behairy@gmail.com>
2019-01-31 00:55:20 +0200

    Display Date in the UI

--- app.js
+++ app.js
@@ -156,7 +156,8 @@ var UIController = (function() {
         expenseLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
         container: '.container',
-        expensesPercLabel: '.item__percentage'
+        expensesPercLabel: '.item__percentage',
+        dateLabel: '.budget__title--month'
     };
     
     var formatNumber = function(num, type) {
@@ -273,6 +274,17 @@ var UIController = (function() {
             
         },
         
+        displayMonth: function() {
+            var now, year, month, months;
+            
+            now = new Date();
+            
+            months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+            month = now.getMonth();
+            year = now.getFullYear();
+            document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ', ' + year;
+        },
+        
         getDOMStrings: function () {
             return DOMstrings;
         }
@@ -382,6 +394,7 @@ var Controller = (function(budgetCtrl, UICtrl) {
     return {
         init: function() {
             console.log('Application has started.');
+            UICtrl.displayMonth();
             UICtrl.displayBudget({
                 budget: 0,
                 totalInc: 0,
