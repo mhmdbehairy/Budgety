0988c8a3f8912135deb80efd522bf0d1a501827d
mhmdbehairy <mhmd.behairy@gmail.com>
2019-01-30 17:45:59 +0200

    Updating the Budget Controller

--- app.js
+++ app.js
@@ -15,6 +15,14 @@ var budgetController = (function() {
         this.value = value;
     };
      
+    var calculateTotal = function(type) {
+        var sum = 0;
+        data.allItems[type].forEach(function(cur) {
+            sum = sum + cur.value;
+        });
+        data.totals[type] = sum;
+    };
+    
     var data = {
         allItems: {
             exp: [],
@@ -23,8 +31,11 @@ var budgetController = (function() {
         totals: {
             exp: 0,
             inc: 0
-        }
-    }
+        },
+        
+        budget: 0,
+        percentage: -1
+    };
     
     return {
         addItem: function(type, des, val) {
@@ -49,6 +60,29 @@ var budgetController = (function() {
             
             // Return the new element
             return newItem;
+        },
+        
+        calculateBudget: function() {
+          
+            // Calculate total income and expenses
+            calculateTotal('exp');
+            calculateTotal('inc');
+            
+            // Calculate the budget: income - expenses
+            data.budget = data.totals.inc - data.totals.exp;
+            
+            // Calculate the percentage of the income that we spent
+            data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+            
+        },
+        
+        getBudget: function() {
+            return {
+                budget: data.budget,
+                totalInc: data.totals.inc,
+                totalExp: data.totals.exp,
+                percentage: data.percentage
+            }
         },
         
         testing: function() {
@@ -149,10 +183,13 @@ var Controller = (function(budgetCtrl, UICtrl) {
     var updateBudget = function() {
         
         // 1. Calculate the budget
+        budgetCtrl.calculateBudget();
         
         // 2. Return the budget
+        var budget = budgetCtrl.getBudget();
         
         // 3. Display the budge on the UI
+        console.log(budget);
         
     };
     
