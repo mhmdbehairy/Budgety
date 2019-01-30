388114be42023f9ec28bc367b9961442f4935d47
mhmdbehairy <mhmd.behairy@gmail.com>
2019-01-30 13:50:32 +0200

    Adding New Item to the Budget Controller

--- app.js
+++ app.js
@@ -3,7 +3,58 @@
 // BUDGET CONTROLLER
 var budgetController = (function() {
     
-    // Some Code
+    var Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+    
+    var Income = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+     
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    }
+    
+    return {
+        addItem: function(type, des, val) {
+            var newItem, ID;
+            
+            // Create new ID
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+            
+            // Create new item based on type
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+            
+            // Push it into our data structure
+            data.allItems[type].push(newItem);
+            
+            // Return the new element
+            return newItem;
+        },
+        
+        testing: function() {
+            console.log(data);
+        }
+    };
     
 })();
 
@@ -56,11 +107,13 @@ var Controller = (function(budgetCtrl, UICtrl) {
     };
     
     var ctrlAddItem = function() {
-       
+        var input, newItem;
+        
         // 1. Get the filed input data
-        var input = UICtrl.getInput();
+        input = UICtrl.getInput();
         
         // 2. Add the item to the budget controller
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
         
         // 3. Add the item to the UI
         
